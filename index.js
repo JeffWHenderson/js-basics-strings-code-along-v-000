18b2fb6312777791c9e244a289de99fe376b188f
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-05 04:27:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 let excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(-3)}!`
+const shortGreeting = `Welcome, ${currentUser.slice(0, -3)}!`
